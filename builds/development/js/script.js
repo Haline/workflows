184077e3e5d86428e99633193ee45dd174a30756
Helena Edin <helena.edin@hiq.se>
2016-03-28 21:23:32 +0200

    Added live reloading

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -5,7 +5,7 @@ $ = require('jquery');
 
 (fill = function(item) {
   return $('.tagline').append("" + item);
-})('Creative minds in Art');
+})('hoho Creative minds in Art');
 
 fill;
 
--- components/coffee/tagline.coffee
+++ components/coffee/tagline.coffee
@@ -1,4 +1,4 @@
 $ = require 'jquery'
-do fill = (item = 'Creative minds in Art') ->
+do fill = (item = 'hoho Creative minds in Art') ->
   $('.tagline').append "#{item}"
 fill
