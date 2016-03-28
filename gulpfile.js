34f1439088fa989e60a71576eba808b20372ce12
Helena Edin <helena.edin@hiq.se>
2016-03-28 21:07:35 +0200

    added watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
 
 var coffeeSource = ['components/coffee/tagline.coffee'];  
 var jsSources = [
-'components/scripts/pixgrid.js',
 'components/scripts/tagline.js',
-'components/scripts/template.js',
+'components/scripts/template.js'
 ];
-
+var sassSource = ['components/sass/style.scss']
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSource)
@@ -24,5 +24,24 @@ gulp.task('js', function(){
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
-})
-		
+});
+	
+gulp.task('compass', function(){
+	gulp.src(sassSource)
+		.pipe(compass({
+			sass: 'components/sass',
+			images: 'builds/development/images',
+			style: 'expanded'
+		})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('builds/development/css'))
+
+});	
+
+gulp.task('watch', function(){
+	gulp.watch(coffeeSource, ['coffee'])
+	gulp.watch(jsSources, ['js'])
+	gulp.watch('components/sass/*.scss', ['compass'])
+});
+
+gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
